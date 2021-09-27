cef62b5f56b0d28dfcaa1129bc8a70c18871f262
aeseva <91491247+aeseva@users.noreply.github.com>
2021-09-27 11:09:56 -0700

    Create chp2-q3 (concatenating variables).py

---
+++ python-grade12/practice questions/chp2/chp2-q3 (concatenating variables).py
@@ -0,0 +1,14 @@
+# ~ Chapter 2: Question 3 ~
+#Take the phrase: twinkle twinkle little star. Store each word in a separate variable, then print out the sentence on one line using print.
+
+word1 = 'twinkle'
+word2 = 'little'
+word3 = 'star'
+
+#this one shows parenthesis' for some reason
+phrase1 = word1, word1, word2, word3
+print(phrase1)
+
+#this one shows the correct, intended thing
+phrase2 = word1 + " " + word1 + " " + word2 + " " + word3
+print(phrase2)
