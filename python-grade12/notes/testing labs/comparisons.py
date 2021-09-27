cdc3f9d8d3ea3e5433560553e423d8e637be5ad2
aeseva <91491247+aeseva@users.noreply.github.com>
2021-09-27 11:08:06 -0700

    Create comparisons.py

---
+++ python-grade12/notes/testing labs/comparisons.py
@@ -0,0 +1,22 @@
+# ~ Evaluating Integer Variables and Print Keyword~
+
+var1 = 1
+var2 = 2
+
+#1 var + var
+show_var1 = var1 + var2
+# is the same as
+#2 var+var
+show_var2 = var1+var2
+
+#1
+print(show_var1)
+#2
+print(show_var2)
+
+# and
+#1-print
+print(var1 + var2)
+# is the same as
+#2-print
+print(var1+var2)
