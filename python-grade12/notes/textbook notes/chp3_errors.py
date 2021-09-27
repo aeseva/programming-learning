ce206b370ddc009412a52d00fa09b414bd0e55ab
aeseva <91491247+aeseva@users.noreply.github.com>
2021-09-27 11:09:10 -0700

    Create chp3_errors.py

---
+++ python-grade12/notes/textbook notes/chp3_errors.py
@@ -0,0 +1,11 @@
+# 3.4.2 Type Error
+wage = input('Type in your current wage per hour. (only insert number, no $) ')
+
+hours = input('Type in the total hours you worked. ')
+
+int(wage)
+int(hours)
+
+#total = wage * hours
+#print(total)
+print(wage, type(wage))
